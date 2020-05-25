6bc6e517e209c482bdc966ffcdc5ec50b57cfb66
Lance Zotigh <lancezotighlz@gmail.com>
2020-05-24 18:57:27 -0600

    Add project files.

---
+++ ParamaterDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ParamaterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pass bu value test:");
+            int testVal1 = 1;
+            Console.WriteLine("Original value: {0}", testVal1);
+            Test(testVal1);
+            Console.WriteLine("Return value: {0}", testVal1);
+        }
+
+        public static void Test(int aValue)
+        {
+            aValue = 11;
+            Console.WriteLine("In test value is {0}", aValue);
+        }
+    }
+}
